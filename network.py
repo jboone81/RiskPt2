94b469ba69eae37538930e4e94edcb756d5a86db
jboone81 <boonej2021@gmail.com>
2025-06-01 11:42:44 -0400

    pt2

--- network.py
+++ network.py
@@ -2,12 +2,26 @@ import socket
 
 class Network:
     def __init__(self):
-        self.client = socket.socket(socket.IF_INET, socket.SOCK_STREAM)
-        self.server = "10.48.8.100"
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server = "10.120.210.166"
         self.port = 5555
         self.addr = (self.server, self.port)
-        self.connect()
+        self.pos = self.connect()
+
+    def getPos(self):
+        return self.pos
+
     
     def connect(self):
-        self.client.connect(self.addr)
-        
+        try:
+            self.client.connect(self.addr)
+            return self.client.recv(2048).decode()
+        except:
+            pass
+
+    def send(self, data):
+        try:
+            self.client.send(str.encode(data))
+            return self.client.recv(2048).decode()
+        except socket.error as e:
+            print(e)
--- sever.py
+++ sever.py
@@ -2,7 +2,7 @@ import socket
 from _thread import *
 import sys
 
-sever = "10.48.8.100"
+sever = "10.120.210.166"
 port = 5555
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
@@ -16,7 +16,7 @@ s.listen(2)
 print("Waiting for a connection, Sever Started")
 
 def threaded_client(conn):
-    
+    conn.send(str.encode("Connected"))
     reply = ""
     while True:
         try:
@@ -38,6 +38,7 @@ def threaded_client(conn):
     print("Lost connection")
     conn.close()
         
+
 while True:
     conn, addr = s.accept()
     print("Connected to:", addr)
