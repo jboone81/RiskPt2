5b835c55f8ce3dbb07ea4323930fd89d28cc6d10
jboone81 <boonej2021@gmail.com>
2025-06-10 21:51:26 -0400

    updated

--- network.py
+++ network.py
@@ -3,7 +3,7 @@ import socket
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "10.120.210.166"
+        self.server = "10.0.0.165"
         self.port = 5555
         self.addr = (self.server, self.port)
         self.pos = self.connect()
--- sever.py
+++ sever.py
@@ -2,7 +2,7 @@ import socket
 from _thread import *
 import sys
 
-sever = "10.120.210.166"
+sever = "10.0.0.165"
 port = 5555
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
@@ -15,22 +15,36 @@ except socket.error as e:
 s.listen(2)
 print("Waiting for a connection, Sever Started")
 
-def threaded_client(conn):
-    conn.send(str.encode("Connected"))
+def read_pos(str):
+    str = str.split(",")
+    return int(str[0]), int(str[1])
+
+def make_pos(tup):
+    return str(tup[0]) + "," + str(tup[1])
+
+pos = [(0,0),(100,100)]
+
+def threaded_client(conn, player):
+    conn.send(str.encode(make_pos(pos[player])))
     reply = ""
     while True:
         try:
-            data = conn.recv(2048)
-            reply = data.decode("utf-8")
-
+            data = read_pos(conn.recv(2048))
+            pos[player] = data 
+            
             if not data:
                 print("Disconnected")
                 break
             else: 
-                print("Received:", reply)
+                if player == 1:
+                    reply = pos[0]
+                else:
+                    reply = pos[1]
+
+                print("Received:", data)
                 print("Sending:", reply)
 
-            conn.sendall(str.encode(reply))
+            conn.sendall(str.encode(make_pos(reply)))
         
         except:
             break
@@ -39,8 +53,10 @@ def threaded_client(conn):
     conn.close()
         
 
+currentPlayer = 0
 while True:
     conn, addr = s.accept()
     print("Connected to:", addr)
 
-    start_new_thread(threaded_client, (conn,))
+    start_new_thread(threaded_client, (conn, currentPlayer))
+    currentPlayer += 1
